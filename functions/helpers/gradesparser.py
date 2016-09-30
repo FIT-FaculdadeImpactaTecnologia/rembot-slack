3886571c5443ead3d96dc2fa0e7a51f608ea291c
Vinicius Luiz <viniciuslcpereira@gmail.com>
2016-09-29 23:03:31 -0300

    parsing grades

--- functions/helpers/gradesparser.py
+++ functions/helpers/gradesparser.py
@@ -1,9 +1,11 @@
 #!usr/bin/env python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+
 import re
 import requests
 from bs4 import BeautifulSoup
 
+import sys  
 class GradesParser(object):
 
     def __init__(self, ra, password, semester):
@@ -40,19 +42,38 @@ class GradesParser(object):
         grades_grid = self.find_grade_grid(response.text)
         return grades_grid
 
-    def parse_grades_table(self):
+    def disciplines_grade_and_dict(self):
+        disciplines_dict = {}
         soup = BeautifulSoup(self.get_grades_table(), 'lxml')
-        # print soup.prettify()
         disciplines = soup.findAll('tr')
-        disciplines = str(disciplines).split('</tr>')
-        print disciplines[-1]
-        # for discipline in disciplines[:-1]:
-            # discipline_soup = BeautifulSoup(discipline, 'lxml')
-            # print discipline_soup.find('td', 'table-left').text
+        disciplines = str(disciplines).split('</tr>')[:-2]
+        for discipline in disciplines[:-1]:
+            discipline_soup = BeautifulSoup(discipline, 'lxml')
+            discipline_name = discipline_soup.find('td', 'table-left').text
+            disciplines_dict[discipline_name] = []
+        return disciplines, disciplines_dict
+
+    def parse_grades(self):
+        grade_and_dict = self.disciplines_grade_and_dict()
+        grades, disciplines_dict = grade_and_dict[0], grade_and_dict[1]   
+        grades_matches = re.findall('\d{1,2},\d{2}', str(grades))
+        print grades_matches
+        # for x in range(len(grades_matches)):
+            # print grades_matches[x]
+            # print grades_matches[x+1]
+            # print grades_matches[x+1]
+
+        # print 'primeiro bimestre: {}\nsegundo bimestre: {}\nmedia final: {}'.format(
+                # grades_matches[0],
+                # grades_matches[1],
+                # grades_matches[4]
+            # )
+        # print disciplines_dict
 
 if __name__ == '__main__':
     ra = '1510512'
     senha = '147071'
     semestre = 3
     parser = GradesParser(ra, senha, semestre)
-    parser.parse_grades_table()   
+    parser.parse_grades()
+    # print parser.disciplines_grade_and_dict()[1]   
