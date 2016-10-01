21eee6c14b5e740b5de2041d1b029aba006e216d
Vinicius Luiz <viniciuslcpereira@gmail.com>
2016-10-01 01:42:51 -0300

    grades parameters validation

--- functions/commands.py
+++ functions/commands.py
@@ -12,8 +12,11 @@ def sala():
     return ':clock7:  {}\n:clock9:  {}\n'.format(salas['fclass'], salas['sclass'])
 
 def notas(params = []):
-    grades = GradesParser(params[0], params[1], params[2])
-    return grades.bot_exec()
+    if(len(params) == 3):
+        grades = GradesParser(params[0], params[1], params[2])
+        return grades.bot_exec()
+    else:
+        return 'Parâmetros inválidos:exclamation:'
 
 def help(command = ''):
     """
